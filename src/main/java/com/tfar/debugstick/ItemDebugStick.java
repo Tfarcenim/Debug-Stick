182e39e405cee2f3738830ab6499803d9df3cd98
Tfarcenim <44327798+Gimpler@users.noreply.github.com>
2019-05-13 01:47:10 -0400

    1.2

--- src/main/java/com/tfar/debugstick/ItemDebugStick.java
+++ src/main/java/com/tfar/debugstick/ItemDebugStick.java
@@ -1,18 +1,21 @@
 package com.tfar.debugstick;
 
 import com.google.common.collect.ImmutableList;
-import com.google.common.collect.ImmutableMap;
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import crafttweaker.mc1120.commands.CommandUtils;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.util.ITooltipFlag;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.entity.passive.EntityWolf;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -20,8 +23,14 @@ import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import net.minecraftforge.oredict.OreDictionary;
 
+import java.awt.datatransfer.*;
+import java.awt.Toolkit;
+
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.tfar.debugstick.Utility.itemDebugStick;
@@ -34,7 +43,10 @@ public class ItemDebugStick extends Item {
     setCreativeTab(CreativeTabs.TOOLS);
   }
 
+  //called when right clicking a block
+
   @Override
+  @Nonnull
   public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
     if (world.isRemote)return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
     ItemStack stack = player.getHeldItemMainhand();
@@ -54,35 +66,31 @@ public class ItemDebugStick extends Item {
         ImmutableList list = world.getBlockState(pos).getBlock().getBlockState().getValidStates();
         int index = list.indexOf(world.getBlockState(pos));
         int newState = (index + 1 >= list.size()) ? 0 : index + 1;
-        world.setBlockState(pos,(IBlockState)list.get(newState));
+        world.setBlockState(pos,(IBlockState)list.get(newState));break;
+      }
+      case 4:{
+        String blockstateInfo = CraftTweakerMC.getBlockState(world.getBlockState(pos)).toString();
+        nbt.setString("CTBlockstate", blockstateInfo);
+        player.sendMessage(new TextComponentString(TextFormatting.WHITE + "Blockstate " + TextFormatting.GREEN + blockstateInfo));
+        List<String> oredicts = Utility.getOreDictOfItem(new ItemStack(Item.getItemFromBlock(world.getBlockState(pos).getBlock()),1,world.getBlockState(pos).getBlock().getMetaFromState(world.getBlockState(pos))));
+        if (!oredicts.isEmpty())
+        player.sendMessage(new TextComponentString(TextFormatting.WHITE + "Oredicts " + TextFormatting.AQUA + oredicts));
+        else player.sendMessage(new TextComponentString(TextFormatting.RED + "No oredicts found"));
+        StringSelection stringSelection = new StringSelection(blockstateInfo);
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.setContents(stringSelection, null);
+        break;
       }
     }
     return super.onItemUse(player, world, pos, hand, facing, hitX, hitY, hitZ);
   }
 
   @Override
-  public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand) {
+  @Nonnull
+  public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player,@Nonnull EnumHand hand) {
     if (world.isRemote)return ActionResult.newResult(EnumActionResult.FAIL,player.getHeldItem(hand));
-    if (player.isSneaking() && world.getBlockState(player.rayTrace(5,1).getBlockPos()).getBlock().isAir(world.getBlockState(player.rayTrace(5,1).getBlockPos()),world,player.rayTrace(5,1).getBlockPos()))Utility.changeMode(player.getHeldItemMainhand());
+    if (player.isSneaking() && world.getBlockState(player.rayTrace(10,1).getBlockPos()).getBlock().isAir(world.getBlockState(player.rayTrace(10,1).getBlockPos()),world,player.rayTrace(5,1).getBlockPos()))Utility.changeMode(player);
     return super.onItemRightClick(world, player, hand);
-  }
-
-  @SubscribeEvent
-  public static void stopGUIs(PlayerInteractEvent.RightClickBlock e){
-  //  if (e.getEntityPlayer().getHeldItemMainhand().getItem() == itemDebugStick)e.setCanceled(true);
-  }
-
-  @SubscribeEvent(priority = EventPriority.HIGHEST)
-  public static void getEntity(PlayerInteractEvent.EntityInteractSpecific e){
-    EntityPlayer p = e.getEntityPlayer();
-    if (p.world.isRemote)return;
-    ItemStack stack = p.getHeldItemMainhand();
-    if (stack.getItem() != itemDebugStick)return;
-    NBTTagCompound nbt = stack.getTagCompound();
-    if (nbt == null)Utility.resetNBT(stack);
-    int mode = nbt.getInteger("mode");
-    if (mode != 1)return;
-    nbt.setString("NBTTagCompound",e.getTarget().toString());
   }
 
   @Override
@@ -97,6 +105,8 @@ public class ItemDebugStick extends Item {
       case 1: {tooltip.add("NBTTagCompound: "+nbt.getString("NBTTagCompound"));break;}
       case 2: {tooltip.add("Blockstate List: "+nbt.getString("BlockstateList"));break;}
       case 3: {tooltip.add("Transform");break;}
+      case 4: {tooltip.add("CT Blockstate:"+nbt.getString("CTBlockstate"));break;}
+      case 5: {tooltip.add("CT Entity:"+nbt.getString("CTEntity"));break;}
     }
     super.addInformation(stack, worldIn, tooltip, flagIn);
   }
--- src/main/java/com/tfar/debugstick/Utility.java
+++ src/main/java/com/tfar/debugstick/Utility.java
@@ -1,10 +1,28 @@
 package com.tfar.debugstick;
 
+import crafttweaker.api.minecraft.CraftTweakerMC;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextFormatting;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.registries.IForgeRegistry;
 
+import java.awt.*;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.StringSelection;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.tfar.debugstick.DebugStick.MODID;
+import static net.minecraft.util.text.TextFormatting.GOLD;
+@Mod.EventBusSubscriber(modid = MODID)
 public class Utility {
   public static ItemDebugStick itemDebugStick = new ItemDebugStick("debugstick");
 
@@ -16,11 +34,56 @@ public class Utility {
     NBTTagCompound nbt = new NBTTagCompound();
     stack.setTagCompound(nbt);
   }
-  public static void changeMode(ItemStack stack){
+  public static void changeMode(EntityPlayer player){
+    ItemStack stack = player.getHeldItemMainhand();
     NBTTagCompound nbt = stack.getTagCompound();
     if (nbt == null)resetNBT(stack);
     int mode = nbt.getInteger("mode");
-    int newMode = (mode > 2) ? 0 : mode + 1;
+    int newMode = (mode > 4) ? 0 : mode + 1;
     nbt.setInteger("mode",newMode);
+    switch (newMode){
+      case 0:{player.sendStatusMessage(new TextComponentString(GOLD + "Blockstate"),true);break;}
+      case 1:{player.sendStatusMessage(new TextComponentString(GOLD + "NBT"),true);break;}
+      case 2:{player.sendStatusMessage(new TextComponentString(GOLD + "Blockstate list"),true);break;}
+      case 3:{player.sendStatusMessage(new TextComponentString(GOLD + "Transform"),true);break;}
+      case 4:{player.sendStatusMessage(new TextComponentString(GOLD + "CrT Blockstate"),true);break;}
+      case 5:{player.sendStatusMessage(new TextComponentString(GOLD + "CrT Entity"),true);break;}
+
+    }
+  }
+  public static List<String> getOreDictOfItem(ItemStack stack) {
+    try {
+      int[] ids = OreDictionary.getOreIDs(stack);
+    List<String> names = new ArrayList<>();
+    for(int id : ids) {
+      names.add(OreDictionary.getOreName(id));
+    }
+    return names;
+    } catch (Exception e){
+      return new ArrayList<>();
+    }
+  }
+
+  @SubscribeEvent
+  public static void getEntity(PlayerInteractEvent.EntityInteractSpecific e) {
+    EntityPlayer p = e.getEntityPlayer();
+    if (p.world.isRemote  ||  e.getHand() != EnumHand.MAIN_HAND)return;
+    ItemStack stack = p.getHeldItemMainhand();
+    if (stack.getItem() != itemDebugStick) return;
+    NBTTagCompound nbt = stack.getTagCompound();
+    if (nbt == null) Utility.resetNBT(stack);
+    int mode = nbt.getInteger("mode");
+    if (mode == 1) {
+      nbt.setString("NBTTagCompound", e.getTarget().toString());
+    }
+    else if (mode == 5) {
+      String s = "<entity:"+CraftTweakerMC.getIEntity(e.getTarget()).getDefinition().getId()+">";
+
+      nbt.setString("CTEntity", s);
+      p.sendMessage(new TextComponentString(TextFormatting.WHITE + "Entity " + TextFormatting.GREEN + s));
+      StringSelection stringSelection = new StringSelection(s);
+      Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+      clipboard.setContents(stringSelection, null);
+    }
   }
 }
